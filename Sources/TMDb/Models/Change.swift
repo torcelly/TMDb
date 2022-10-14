ae9bb242bf6267fd6351335f33faa675e188b7d4
Fernando Torcelly <fernando@torcelly.com>
2022-10-14 14:53:36 +0100

    Fixes the value of adults that may be null

--- Sources/TMDb/Models/Change.swift
+++ Sources/TMDb/Models/Change.swift
@@ -13,14 +13,14 @@ public struct Change: Identifiable, Decodable, Equatable, Hashable {
     /// Movie identifier.
     public let id: Int
     /// Is the item only suitable for adults.
-    public let adult: Bool
+    public let adult: Bool?
 
     /// Creates a new `Change`.
     ///
     /// - Parameters:
     ///    - id: Item identifier.
     ///    - adult: Is the item only suitable for adults.
-    public init(id: Int, adult: Bool) {
+    public init(id: Int, adult: Bool?) {
         self.id = id
         self.adult = adult
     }
